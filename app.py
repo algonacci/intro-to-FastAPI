e0bb0c1f9b2c78bdc2078b6238ba4b07d6e15d26
Zetta Eric Julianto <algonacci1@gmail.com>
2022-08-04 07:55:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from fastapi import FastAPI
 from typing import Union
 from pydantic import BaseModel
@@ -9,6 +10,12 @@ class Item(BaseModel):
     name: str
     price: int
     is_offer: Union[bool, None] = None
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    category: str
 
 
 @app.get("/")
@@ -24,3 +31,8 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id, "item_price": item.price}
+
+
+@app.post('/post/{post_id}')
+def post_something(post_id: int, post: Post):
+    return {"post_id": post_id, "title": post.title, "content": post.content, "category": post.category}
